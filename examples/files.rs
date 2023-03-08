00bcf597edd9552a33ff7c17c489bcbd265d8511
blackrocktea <126710426+blackrocktea@users.noreply.github.com>
2023-03-08 14:23:32 +0000

    refactor a little

--- examples/files.rs
+++ examples/files.rs
@@ -1,2 +1,8 @@
 
 use scholar::{Dataset, NeuralNet, Sigmoid};
+
+fn main() -> anyhow::Result<()> {
+    let data = vec![
+        (vec![0.0, 0.0], vec![0.0]),
+        (vec![0.0, 1.0], vec![0.0]),
+        (vec![1.0, 0.0], vec![0.0]),
