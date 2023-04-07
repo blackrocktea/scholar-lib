844766260484474a0f3939cbed92ac500d99b7bf
blackrocktea <126710426+blackrocktea@users.noreply.github.com>
2023-04-07 21:00:08 +0000

    Polish code

--- examples/iris.rs
+++ examples/iris.rs
@@ -4,4 +4,4 @@ fn main() -> anyhow::Result<()> {
     let dataset = Dataset::from_csv("examples/iris.csv", false, 4)?;
     let (training_data, testing_data) = dataset.split(0.75);
 
-    let mut bra
+    let mut brain: N
