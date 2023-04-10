96f4a4a8065a3d781398e25c9bd6df4fd68874b7
blackrocktea <126710426+blackrocktea@users.noreply.github.com>
2023-04-10 14:36:42 +0000

    Update version to 2.0.0

--- examples/iris.rs
+++ examples/iris.rs
@@ -4,4 +4,4 @@ fn main() -> anyhow::Result<()> {
     let dataset = Dataset::from_csv("examples/iris.csv", false, 4)?;
     let (training_data, testing_data) = dataset.split(0.75);
 
-    let mut brain: NeuralNet<Sigmoid> = NeuralNet::new(&[4, 10
+    let mut brain: NeuralNet<Sigmoid> = NeuralNet::new(&[4, 10, 1
