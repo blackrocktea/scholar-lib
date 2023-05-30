f87ece2f0d1a33ae24d13cbfe60633909d21e86f
blackrocktea <126710426+blackrocktea@users.noreply.github.com>
2023-05-30 14:58:51 +0000

    add some translations

--- examples/xor.rs
+++ examples/xor.rs
@@ -13,4 +13,4 @@ fn main() {
     let mut brain: NeuralNet<Sigmoid> = NeuralNet::new(&[2, 10, 10, 1]);
     brain.train(dataset, 250_000, 0.01);
 
-    println!("Predict
+    println!("Prediction: {
