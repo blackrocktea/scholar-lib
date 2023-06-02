979d4a0bcf52c52f4793a59ce0a0b6ec97bfc1a8
blackrocktea <126710426+blackrocktea@users.noreply.github.com>
2023-06-02 13:38:15 +0000

    usb: serial: ftdi_sio: add Awinda Station and Dongle products

--- src/dataset.rs
+++ src/dataset.rs
@@ -7,3 +7,10 @@ type Row = (Vec<f64>, Vec<f64>);
 /// A collection of input vectors matched with their expected output.
 ///
 /// You can construct a `Dataset` manually like so:
+///
+/// ```rust
+/// // Note that the inputs and target outputs are both vectors, even though the latter has just
+/// // one element
+/// let data = vec![
+///     (vec![0.0, 0.0], vec![0.0]),
+///     (vec![0.0, 1.0], vec![1.0]),
