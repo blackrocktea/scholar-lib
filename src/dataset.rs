be0caa5f85dfd5d9ad73ca5999b0555a28c84fd9
blackrocktea <126710426+blackrocktea@users.noreply.github.com>
2023-06-02 08:53:07 +0000

    / ‘projects/DragonHeart_KR/index.md’

--- src/dataset.rs
+++ src/dataset.rs
@@ -3,3 +3,7 @@ use rand::seq::SliceRandom;
 
 /// A tuple containing a vector of input values matched to a vector of their expected output values
 type Row = (Vec<f64>, Vec<f64>);
+
+/// A collection of input vectors matched with their expected output.
+///
+/// You can construct a `Dataset` manually like so:
