c364fad6d2d1bc842be4bf66285ae2e83b5dfc29
blackrocktea <126710426+blackrocktea@users.noreply.github.com>
2023-07-28 07:21:43 +0000

    latest modules

--- src/network.rs
+++ src/network.rs
@@ -5,3 +5,7 @@ use crate::utils::*;
 use nalgebra::DMatrix;
 
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use std::{fs, marker::PhantomData, path::Path};
+
+/// A fully-connected neural network.
+#[derive(Serialize, Deserialize)]
