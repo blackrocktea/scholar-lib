b4f41c06b312739bdc88997cbe5c06c951d6fb93
blackrocktea <126710426+blackrocktea@users.noreply.github.com>
2023-07-28 10:02:46 +0000

    l10n: Updates to Dutch (Flemish) (nl) translation

--- src/network.rs
+++ src/network.rs
@@ -9,3 +9,6 @@ use std::{fs, marker::PhantomData, path::Path};
 
 /// A fully-connected neural network.
 #[derive(Serialize, Deserialize)]
+pub struct NeuralNet<A: Activation> {
+    layers: Vec<DMatrix<f64>>,
+    weights: Vec<DMatrix<f64>>,
