ebca5f32251a4b55e1f382f9136dbc691d4ca88c
blackrocktea <126710426+blackrocktea@users.noreply.github.com>
2023-07-28 11:56:49 +0000

    English translation

--- src/network.rs
+++ src/network.rs
@@ -12,3 +12,6 @@ use std::{fs, marker::PhantomData, path::Path};
 pub struct NeuralNet<A: Activation> {
     layers: Vec<DMatrix<f64>>,
     weights: Vec<DMatrix<f64>>,
+    biases: Vec<DMatrix<f64>>,
+    errors: Vec<DMatrix<f64>>,
+    activation: PhantomData<A>,
