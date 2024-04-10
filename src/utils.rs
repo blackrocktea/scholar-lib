6c68a168af4851456f90b4bb1325032d960e36c7
blackrocktea <126710426+blackrocktea@users.noreply.github.com>
2024-04-10 16:18:44 +0000

    pedantic

--- src/utils.rs
+++ src/utils.rs
@@ -4,3 +4,6 @@ use rand::distributions::{Distribution, Uniform};
 
 /// Generates a matrix with the specified dimensions and random values between -1 and 1.
 pub(crate) fn gen_random_matrix(rows: usize, cols: usize) -> DMatrix<f64> {
+    let elements = rows * cols;
+    let range = Uniform::new_inclusive(-1.0, 1.0);
+    DMatrix::from_iterator(
